53e34ac21c63f728b5f60ba051c190df04247fc5
Antonino Catinello <antonino@catinello.eu>
2016-05-27 16:05:41 +0200

    performence data/version information

--- main.go
+++ main.go
@@ -24,16 +24,35 @@ const (
 // export NCG2=debug
 const DEBUG = "NCG2"
 
+// license information
+const (
+        author = "Antonino Catinello"
+        license = "BSD"
+        year = "2016"
+        copyright = "\u00A9"
+)
+
+
 var (
 	// command line arguments
 	link *string
 	user *string
 	pass *string
+	version *bool
 	// using ssl to avoid name conflict with tls
-	ssl  *bool
+	ssl *bool
 	// env debugging variable
 	debug string
+	// performence data
+	pdata string
+	// version value
+	id string
 )
+
+// handle performence data output
+func perf(elapsed, total, inputs, tput, index float64) {
+	pdata = fmt.Sprintf("time=%f;;;; total=%.f;;;; sources=%.f;;;; throughput=%.f;;;; index_failures=%.f;;;;", elapsed, total, inputs, tput, index)
+}
 
 // handle args
 func init() {
@@ -41,7 +60,9 @@ func init() {
 	user = flag.String("u", "", "API username")
 	pass = flag.String("p", "", "API password")
 	ssl = flag.Bool("insecure", false, "Accept insecure SSL/TLS certificates.")
+	version = flag.Bool("version", false, "Display version and license information.")
 	debug = os.Getenv(DEBUG)
+	perf(0, 0, 0, 0, 0)
 }
 
 // return nagios codes on quit
@@ -65,7 +86,7 @@ func quit(status int, message string, err error) {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("%s - %s|\n", ev, message)
+	fmt.Printf("%s - %s|%s\n", ev, message, pdata)
 	os.Exit(status)
 }
 
@@ -90,21 +111,40 @@ func parse(link *string) string {
 func main() {
 	flag.Parse()
 
+	if *version {
+		fmt.Printf("Version: %v License: %v %v %v %v\n", id, license, copyright, year, author)
+		os.Exit(3)
+	}
+
 	if len(*user) == 0 || len(*pass) == 0 {
 		flag.PrintDefaults()
 		os.Exit(3)
 	}
 
 	c := parse(link)
-
 	start := time.Now()
-	m := query(c+"/index.json", *user, *pass)
-	fmt.Println(m["id"].(float64))
-	fmt.Println(m["name"].(string))
-	fmt.Println(m["tags"].([]interface{}))
+
+	system := query(c+"/system", *user, *pass)
+	if system["is_processing"].(bool) != true {
+		quit(CRITICAL, "Service is not processing!", nil)
+	}
+	if strings.Compare(system["lifecycle"].(string), "running") != 0 {
+		quit(WARNING, fmt.Sprintf("lifecycle: %v", system["lifecycle"].(string)), nil)
+	}
+	if strings.Compare(system["lb_status"].(string), "alive") != 0 {
+		quit(WARNING, fmt.Sprintf("lb_status: %v", system["lb_status"].(string)), nil)
+	}
+
+	index := query(c+"/system/indexer/failures", *user, *pass)
+	tput := query(c+"/system/throughput", *user, *pass)
+	inputs := query(c+"/system/inputs", *user, *pass)
+	total := query(c+"/count/total", *user, *pass)
+
 	elapsed := time.Since(start)
 
-	quit(OK, fmt.Sprintf(" took %v", elapsed), nil)
+	perf(elapsed.Seconds(), total["events"].(float64), inputs["total"].(float64), tput["throughput"].(float64), index["total"].(float64))
+	quit(OK, fmt.Sprintf("Service is running!\n%.f total events processed\n%.f index failures\n%.f throughput\n%.f sources\nCheck took %v",
+		total["events"].(float64), index["total"].(float64), tput["throughput"].(float64), inputs["total"].(float64), elapsed), nil)
 }
 
 // call Graylog2 HTTP API
@@ -127,10 +167,10 @@ func query(target string, user string, pass string) map[string]interface{} {
 	req.SetBasicAuth(user, pass)
 
 	res, err := client.Do(req)
-	defer res.Body.Close()
 	if err != nil {
 		quit(CRITICAL, "Can not connect to Graylog2 API", err)
 	}
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
